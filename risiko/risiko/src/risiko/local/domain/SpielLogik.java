cecb2773016a4fa4217022f4e796733cfa8a6972
Casia <119851945+Casia03@users.noreply.github.com>
2024-04-20 15:44:15 +0200

    commit

--- risiko/risiko/src/risiko/local/domain/SpielLogik.java
+++ risiko/risiko/src/risiko/local/domain/SpielLogik.java
@@ -7,18 +7,28 @@ public class SpielLogik {
 
     }
     
-    public void attack(Land vonLand, Land nachLand){ 
+    public void attack(Land vonLand, Land nachLand, int[] wurfel){ 
         
+
+        int[] resultat = wurfel(vonLand,nachLand);
+
     }
 
     private int[] wurfel(Land vonLand, Land nachLand){
-        int wurfel[];
+        int[] wurfel= new int[5]; // Wuerfel wird ein Array sein der aus 5 Zellen bestehen wird, die erste Drei fur den Angreifer und die letztee Zwei fur den Verteidiger
         int maxAttackArmee = vonLand.getArmee(); // wieviele Armee stehen fur den Angriff zur verfuegung
         int maxDefendArmee = nachLand.getArmee(); // wieviele Armee steht fur die Verteidigung zur verfuegung
 
+        //oder 
+
+        //int attackWuerfel[x]; // wobei x und y die groesse des wurfel arrays darstellen wird, 
+        //int defenceWuerfel[y];
+
         if(maxAttackArmee <= 1){
-           //kann nicht von heir angreifen exception
+           //kann nicht von hier angreifen exception, weil zu wenig Armee
         }
+
+        
 
         return wurfel;
     }
