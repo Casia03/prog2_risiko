8ddef7f0e78f53fd4f437c6da0a2cf06160d662c
Casia <119851945+Casia03@users.noreply.github.com>
2024-04-20 16:30:03 +0200

    comit

--- risiko/risiko/src/risiko/local/domain/SpielLogik.java
+++ risiko/risiko/src/risiko/local/domain/SpielLogik.java
@@ -12,24 +12,61 @@ public class SpielLogik {
 
         int[] resultat = wurfel(vonLand,nachLand);
 
+        if(resultat != 0){
+
+        }
+
     }
 
-    private int[] wurfel(Land vonLand, Land nachLand){
-        int[] wurfel= new int[5]; // Wuerfel wird ein Array sein der aus 5 Zellen bestehen wird, die erste Drei fur den Angreifer und die letztee Zwei fur den Verteidiger
-        int maxAttackArmee = vonLand.getArmee(); // wieviele Armee stehen fur den Angriff zur verfuegung
-        int maxDefendArmee = nachLand.getArmee(); // wieviele Armee steht fur die Verteidigung zur verfuegung
+    private int[] wurfel(Land vonLand, Land nachLand){ //Methode zur erzeugubg von zufaeliger wurfelnergebnissen
+        if (angriffMoeglich(vonLand, nachLand)){
+            
+            int[] wurfel= new int[5]; // Wuerfel wird ein Array sein der aus 5 Zellen bestehen wird, die erste Drei fur den Angreifer und die letztee Zwei fur den Verteidiger
+            int maxAttackArmee = vonLand.getArmee(); // wieviele Armee stehen fur den Angriff zur verfuegung
+            int maxDefendArmee = nachLand.getArmee(); // wieviele Armee steht fur die Verteidigung zur verfuegung
 
-        //oder 
+            //oder 
 
-        //int attackWuerfel[x]; // wobei x und y die groesse des wurfel arrays darstellen wird, 
-        //int defenceWuerfel[y];
+            //int attackWuerfel[x]; // wobei x und y die groesse des wurfel arrays darstellen wird, 
+            //int defenceWuerfel[y];
 
-        if(maxAttackArmee <= 1){
-           //kann nicht von hier angreifen exception, weil zu wenig Armee
+            if(maxAttackArmee <= 1){
+            //kann nicht von hier angreifen exception, weil zu wenig Armee
+                return wurfel;   
+            }
+
+            if(maxAttackArmee >= 4){         //Angreifen mit 3 Armeen, 3 wurfel
+
+            }else if (maxAttackArmee >= 3){  //Angreifen mit 2 Armeen, 2 wurfel
+
+            }else if (maxAttackArmee == 2){  //Angreifen mit 1 Armee, 1 wurfel
+
+            }
+
+            if(maxDefendArmee >=2){
+
+            }else{
+
+            }
+        
+        }else{
+            //kann nicht sein eigenes land angreifen exception
+        }
+
+        return wurfel;
+    }
+
+    private boolean angriffMoeglich(Land vonLand, Land nachLand){ //Methode zum pruefen ob der Angreifer sein eigenes Land angreift
+        boolean angriffmoeglich = false;
+        int angreifer = vonLand.getSpielerID();
+        int verteidiger = nachLand.getSpielerID();
+
+        if(angreifer != verteidiger){
+            return true;
+        }else{
+            return false;
         }
 
         
-
-        return wurfel;
     }
 }
